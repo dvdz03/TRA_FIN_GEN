c089deaf6fd624a19e626cbc86168555c49cdb40
dvdz03 <dflores04@alumnos.uaq.mx>
2025-05-25 11:02:45 -0600

    RED

--- SCRIPTS/SCRIPT_TRES.R
+++ SCRIPTS/SCRIPT_TRES.R
@@ -1,3 +1,9 @@
 ##############################
 #### ESTE ES PARA LA RED #####
 ##############################
+
+miRNAs <- get_multimir(org = "hsa", target = "TP53", table = "predicted")
+miRNABONITO <- miRNAs@data %>% filter(score > 0.8)
+plot(graph_from_data_frame(miRNABONITO), layout = layout_with_fr)
+head(miRNABONITO)
+#AHÍ ESTÁ LA RED, SOLO FALTA EDITARLA PARA QUE SE VEA BIEN Y TODO ESO 
