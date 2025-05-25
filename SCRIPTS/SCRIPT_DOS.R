1580f5735258efc77e49356f4e2ca4aa599c870d
dvdz03 <dflores04@alumnos.uaq.mx>
2025-05-25 10:32:26 -0600

    heatmap

--- SCRIPTS/SCRIPT_DOS.R
+++ SCRIPTS/SCRIPT_DOS.R
@@ -1,3 +1,10 @@
 #################################
 #### ESTE ES PARA EL HEATMAP ####
 #################################
+
+paraheat <- data.frame(Subtype = subtipos_factor)
+rownames(paraheat) <- colnames(genes_top_limpios)
+pheatmap(mat = genes_top_limpios,
+         annotation_col = paraheat,
+         show_colnames = FALSE,
+         color = colorRampPalette(c("orange","black","darkorchid"))(100))
