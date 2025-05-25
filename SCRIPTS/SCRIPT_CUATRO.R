8076dc235b0d05888abd7ce0e7f432f2592161df
dvdz03 <dflores04@alumnos.uaq.mx>
2025-05-25 11:22:08 -0600

    ontologia

--- SCRIPTS/SCRIPT_CUATRO.R
+++ SCRIPTS/SCRIPT_CUATRO.R
@@ -1,3 +1,29 @@
 ################################
 #### ESTE ES PARA ONTOLOGÍA ####
 ################################
+
+genes_significativos <- resultados %>% 
+  filter(abs(logFC)>1 & adj.P.Val > 0.05)%>%
+  rownames ()
+#para saber que tipo de genes tengo
+head(genes_significativos)
+keytypes(org.Hs.eg.db)# esto es para ver las opciones que puedo hacer
+
+genes_buenos <- sub("\\..*","", rownames(resultados))
+genes_ya <- bitr(genes_buenos,
+                 fromType = "ENSEMBL",
+                 toType = "ENTREZID",
+                 OrgDb = "org.Hs.eg.db")
+head(genes_ya)
+cosago <- enrichGO( gene = genes_ya$ENTREZID,
+                    OrgDb = org.Hs.eg.db,
+                    ont = "BP",
+                    pAdjustMethod = "BH",
+                    pvalueCutoff = 0.05,
+                    readable = TRUE)
+head(cosago)
+barplot(cosago,
+        showCategory = 15, 
+        title= "ontología (PROCESO BIOLÓGICO)",
+        font.size = 8)
+
