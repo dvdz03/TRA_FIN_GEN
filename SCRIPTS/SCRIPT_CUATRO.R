cd7988fbb845ed3bdeb31dd59f690bf2f350a456
dvdz03 <dflores04@alumnos.uaq.mx>
2025-05-25 19:52:44 -0600

    LAS ONTOLOGÍAS POR CATEGORÍA

--- SCRIPTS/SCRIPT_CUATRO.R
+++ SCRIPTS/SCRIPT_CUATRO.R
@@ -17,13 +17,39 @@ genes_ya <- bitr(genes_buenos,
 head(genes_ya)
 cosago <- enrichGO( gene = genes_ya$ENTREZID,
                     OrgDb = org.Hs.eg.db,
-                    ont = "ALL",
+                    ont = "BP",
                     pAdjustMethod = "BH",
                     pvalueCutoff = 0.05,
                     readable = TRUE)
 head(cosago)
-barplot(cosago,
-        showCategory = 50, 
-        title= "ontología (FUNCIÓN MOLECULAR, PROCESO BIOL Y EL OTRO)",
-        font.size = 1)
+funbio <- barplot(cosago,
+        showCategory = 20, 
+        title= "ontología (Proceso biológico)",
+        font.size = 7)
+funbio
+
+molecular <- enrichGO( gene = genes_ya$ENTREZID,
+                       OrgDb = org.Hs.eg.db,
+                       ont = "MF",
+                       pAdjustMethod = "BH",
+                       pvalueCutoff = 0.05,
+                       readable = TRUE)
+mole <- barplot(molecular,
+                showCategory = 20,
+                title = "ontología (Función Molecular)",
+                font.size = 5)
+mole
+
+elotro <- enrichGO(gene = genes_ya$ENTREZID,
+                   OrgDb = org.Hs.eg.db,
+                   ont = "CC",
+                   pAdjustMethod = "BH",
+                   pvalueCutoff = 0.05,
+                   readable = TRUE)
+otro <- barplot(elotro, 
+                showCategory = 20,
+                title = "ontología (COMPARTIMENTO CELULAR)",
+                font.size = 5)
+
+otro
 
